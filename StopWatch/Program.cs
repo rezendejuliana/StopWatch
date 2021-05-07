f7642a82e99e2745981db885f5c7543639c9989f
Juliana <juliana@rarolabs.com.br>
2021-05-06 21:26:52 -0300

    Stop Watch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StopWatch
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StopWatch stopWatch = new StopWatch();
+
+            stopWatch.Start();
+            System.Threading.Thread.Sleep(2000);
+            stopWatch.Stop();
+            //stopWatch.Start();
+            //System.Threading.Thread.Sleep(2000);
+             //stopWatch.Stop();
+            Console.WriteLine(stopWatch.Duration());
+
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StopWatch
+{
+    partial class Program
+    {
+        public class StopWatch
+        {
+            private TimeSpan _duration;
+            private DateTime _timeStart { get; set; }
+            private DateTime _timeStop { get; set; }
+            private bool _flgStart = false;
+            
+            public void Start()
+            {
+                if (!_flgStart)
+                {
+                    _flgStart = true;
+                    _timeStart = DateTime.Now;
+                }
+                else
+                {
+                    throw new InvalidOperationException("StopWatch Already running");
+                }
+
+            }
+
+            public void Stop()
+            {
+                if (_flgStart) {
+                    _flgStart = false;
+                    _timeStop = DateTime.Now;
+               }
+                
+            }
+
+            public TimeSpan Duration()
+            {
+                _duration = TimeSpan.Parse(Convert.ToString(_timeStop - _timeStart));
+                return _duration ;
+                    
+            }
+
+            
+
+        }
+    }
+}
